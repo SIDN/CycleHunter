0285dfe493fb776a80c43656773291156d4cd2b0
Stefan Ubbink <Stefan.Ubbink@sidn.nl>
2021-02-12 15:50:55 +0100

    Split the fields on all white spaces, not just space and tab and cleanup the code

--- largeZoneParser.py
+++ largeZoneParser.py
@@ -1,30 +1,28 @@
 # this file reads large zone fields and extract its set of NS records
-
 import argparse
 import logging
 import sys
+import re
 
 
-#method that detects if the zone file is tab or space seprated
 def parseNS(s, extension=None):
-
-    sp = s.lower().split('\t')
-    ns_entry = 0
-
-    #if len==1, then file is not tab separated, and it is space sep
-    if len(sp)==1:
-        sp=s.lower().split(" ")
-
-    foundNS=False
-    for item in sp:
-        if item=='ns' and foundNS==False and 'rrsig' not in s.lower() and 'nsec' not in s.lower():
-            ns_entry = sp[-1].rstrip()
-            if ns_entry[-1] != ".":
-                ns_entry = ns_entry + extension
-                foundNS=True
+    ns_entry = '0'
+    if s[0] != ";":
+        sp = re.split('[\s]+', s.lower())
+        foundNS = False
+        for item in sp:
+            if item == 'ns' and foundNS is False and 'rrsig' not in s.lower() and 'nsec' not in s.lower() and foundNS is False:
+                ns_entry = sp[-1].rstrip()
+                if len(ns_entry) == 0:
+                    # test if the one before the last has NS
+                    tempNS = sp[-2].strip()
+                    if "." in tempNS:
+                        ns_entry = tempNS
+                if ns_entry[-1] != ".":
+                    ns_entry = ns_entry + extension
+                    foundNS = True
 
     return ns_entry
-
 
 
 def get_ns_set(zonefile=None, extension=None):
@@ -36,8 +34,8 @@ def get_ns_set(zonefile=None, extension=None):
 
     with open(zonefile) as f:
         for line in f:
-            ns_entry=parseNS(line,extension)
-            if ns_entry!=0:
+            ns_entry = parseNS(line, extension)
+            if ns_entry != '0':
                 nsset.add(ns_entry)
     return nsset
 
@@ -45,7 +43,7 @@ def get_ns_set(zonefile=None, extension=None):
 def zone_parser(zonefile=None, zonename=None, output_file=None):
     nsset = get_ns_set(zonefile=zonefile, extension=zonename)
 
-    if len(nsset) >0:
+    if len(nsset) > 0:
         with open(output_file, 'w') as aus:
             for k in nsset:
                 aus.write(f"{k}\n")
@@ -53,6 +51,8 @@ def zone_parser(zonefile=None, zonename=None, output_file=None):
         logging.info('Error with largeZoneParser.py: could not extract NS records from zone file')
         logging.info('Please run CycleHunter step-by-step and changeZoneParser.py to your zone synthax')
         sys.exit(output_file + "  has no NS records; stop here. Plase check if largeZoneParser correctly parsers your zone file")
+
+
 if __name__ == '__main__':
     # Setup logging if called from command line
     logging.basicConfig(filename='zone-parser.log',
